2da530a0c963c89caf7570949a183ff4f70d7ba5
Anand raja <46064269+actionanand@users.noreply.github.com>
2026-06-28 14:14:24 +0000

    update dev environment file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  tmdbApiReadToken: 'add_your_own_api_key',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p',
+  GOOGLE_SHEET_ID: '1OZZ3XTy1OglXUrHOfmtpMMd9tDqyMlx4OJ8TLnKk310', // Replace with actual Sheet ID from URL
+  SHEET_GID: 357048143, // Sheet tab number (0 for first tab)
+  passwordHash: '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', // Default hash for 'password'
+};
